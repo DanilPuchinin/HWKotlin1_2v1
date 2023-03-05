ffd2979b009dd14e2dd982525429957bea946e9f
Danil <cronhorm@mail.ru>
2023-03-05 12:31:55 +0300

    Impliment commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,10 @@
+fun main(){
+    val likes = 1253
+    if (likes % 100 === 11) {
+        println("Понравилось " + likes + " людям")
+    } else if (likes % 10 === 1) {
+        println("Понравилось " + likes + " человеку")
+    } else {
+        println("Понравилось " + likes + " людям")
+    }
+}
